65791c1bcb82a853e69d1688e0c6422b4b3a9903
arma <arma.malekii@gmail.com>
2025-10-12 20:18:48 +0330

    start add notification

--- app/Http/Controllers/Dashboard/NotificationController.php
+++ app/Http/Controllers/Dashboard/NotificationController.php
@@ -3,15 +3,19 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Dashboard\Notification\NotificationCollection;
+use App\Models\AppNotification;
 use Inertia\Inertia;
 
 class NotificationController extends Controller
 {
     public function index()
     {
-        $notifications = auth()->user()->notifications()->paginate(10);
+        $notifications = AppNotification::where('user_id', auth()->id())
+            ->latest()
+            ->paginate(10);
         return Inertia::render('dashboard/notifications/index', [
-            'notificationsList' => $notifications,
+            'notificationsList' => new NotificationCollection($notifications),
         ]);
     }
 }
--- resources/js/pages/dashboard/notifications/index.tsx
+++ resources/js/pages/dashboard/notifications/index.tsx
@@ -1,9 +1,10 @@
+import { Card, CardContent } from '@/components/ui/card';
 import AppLayout from '@/layouts/app-layout';
 import { dashboard } from '@/routes';
 import type { BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
-import { Card } from '@/components/ui/card';
-import notifications from '@/routes/notifications';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Terminal } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -12,15 +13,44 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
     {
         title: 'Notifications',
-        href: notifications.index(),
+        href: '#',
     },
 ];
-export default function () {
+
+interface Notification{
+    id:number;
+    title:string;
+    message:string;
+}
+interface NotificationPageProps {
+    notificationsList: {
+        data: Notification[];
+    };
+}
+export default function ({ notificationsList }: NotificationPageProps) {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <Card>
-                اینجا به نظرم بهتره ویدو قرار بگیره
+                <CardContent className={`space-y-4`}>
+                {notificationsList.data.length > 0 ? (
+                    notificationsList.data.map((notic, index) => (
+                        <Alert
+                            key={index}
+                            variant="default">
+                            <Terminal />
+                            <AlertTitle>{notic.title}</AlertTitle>
+                            <AlertDescription>
+                                {notic.message}
+                            </AlertDescription>
+                        </Alert>
+                    ))
+                ) : (
+                    <>Null</>
+                )}
+                </CardContent>
+                حتما باید رنگ نوتیف و ایکونش رو هم مشخص کنی
+
             </Card>
         </AppLayout>
     );
